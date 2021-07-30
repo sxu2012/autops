0b12ce935c2b25a15eb769cb5b8f07c769f669a8
Jane Xu <sxu2012@gmail.com>
2021-07-30 12:16:46 -1000

    modified bash file, map-reduce job failed the same way

--- mapreduce1.sh
+++ mapreduce1.sh
@@ -5,4 +5,4 @@
 #-output /autops/output/all_accidents
 
 #-file option is deprecated, with or without produces the same error result
-hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar -mapper /home/hdfs/mapper1.py -reducer /home/hdfs/reducer1.py -input /user/jxu/data.csv -output /user/jxu/all_accidents
+hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar -file /home/hdfs/mapper1.py -mapper /home/hdfs/mapper1.py -file /home/hdfs/reducer1.py -reducer /home/hdfs/reducer1.py -input /user/jxu/data.csv -output /user/jxu/all_accidents
--- mapreduce2.sh
+++ mapreduce2.sh
@@ -1,10 +1,5 @@
-#hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
-#-file /home/hdfs/mapper2.py    -mapper /home/hdfs/mapper2.py \
-#-file /home/hdfs/reducer2.py   -reducer /home/hdfs/reducer2.py \
-#-input output/all_accidents \
-#-output output/make_year_count
 hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
-   -mapper /home/hdfs/mapper2.py \
-   -reducer /home/hdfs/reducer2.py \
-   -input output/all_accidents \
-   -output output/make_year_count
+ -file /home/hdfs/mapper2.py    -mapper /home/hdfs/mapper2.py \
+ -file /home/hdfs/reducer2.py   -reducer /home/hdfs/reducer2.py \
+ -input output/all_accidents \
+ -output output/make_year_count
